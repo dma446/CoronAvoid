7749300109680ea5aa55c392bfca84367c9318fd
dma446 <dma446@nyu.edu>
2020-04-03 21:30:01 -0400

    Initial Commit

---
+++ CoronAvoid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoronAvoid
+//
+//  Created by David Acheampong on 4/3/20.
+//  Copyright © 2020 NYU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
