5a51ed547bf3e2715879c167959bf75022d69ed2
Josh <jz2450@nyu.edu>
2020-04-30 19:43:13 -0400

    remove pod folder from git

--- CoronAvoid/AppDelegate.swift
+++ CoronAvoid/AppDelegate.swift
@@ -15,12 +15,24 @@ import GoogleSignIn
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    let notificationCenter = UNUserNotificationCenter.current()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         //Initialize sign-in
         GIDSignIn.sharedInstance().clientID = FirebaseApp.app()?.options.clientID
+        
+        //notifications
+        notificationCenter.delegate = self
+        
+        let options: UNAuthorizationOptions = [.alert, .sound, .badge]
+        notificationCenter.requestAuthorization(options: options) {
+            (didAllow, error) in
+            if !didAllow {
+                print("User has declined notifications")
+            }
+        }
         return true
     }
 
@@ -45,6 +57,49 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         return GIDSignIn.sharedInstance().handle(url)
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    
+    
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                willPresent notification: UNNotification,
+                                withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        completionHandler([.alert, .sound])
+    }
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                didReceive response: UNNotificationResponse,
+                                withCompletionHandler completionHandler: @escaping () -> Void) {
+        
+        if response.notification.request.identifier == "Local Notification" {
+            print("Handling notifications with the Local Notification Identifier")
+        }
+        
+        completionHandler()
+    }
+    
+    func sendNotification() {
+        
+        let notificationContent = UNMutableNotificationContent()
+        notificationContent.title = "Another User Detected"
+        notificationContent.body = "Looks like you're in someone else's bubble"
+        notificationContent.sound = UNNotificationSound(named: UNNotificationSoundName(rawValue: "cough.wav"))
+        
+        // need to change trigger to CLocation/Beacon instead of time interval
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+        
+        let identifier = "Local Notification"
+        let request = UNNotificationRequest(identifier: identifier, content: notificationContent, trigger: trigger)
+
+        notificationCenter.add(request) { (error) in
+            if let error = error {
+                print("Error \(error.localizedDescription)")
+            }
+        }
     }
 }
 
--- CoronAvoid/DistanceViewController.swift
+++ CoronAvoid/DistanceViewController.swift
@@ -10,23 +10,65 @@ import UIKit
 
 class DistanceViewController: UIViewController {
 
+    @IBOutlet weak var beaconSwitch: UISwitch!
+    
+    let appDelegate = UIApplication.shared.delegate as? AppDelegate
+    
+    public var screenWidth: CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    public var screenHeight: CGFloat {
+        return UIScreen.main.bounds.height
+    }
+    
+    var radarImage:UIImage?
+    var isAnimating:Bool?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        if beaconSwitch.isOn {
+            isAnimating = true
+        }
+        else if !beaconSwitch.isOn {
+            isAnimating = false
+        }
+        
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(onTimer), userInfo: nil, repeats: true)
     }
     
     @IBAction func closeVC(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-    @IBAction func radarSwitchFlipped(_ sender: Any) {
-        //push current date to database user profile
-        
-        
-        //start background processes here
-        
-        
+    @IBAction func beaconSwitchFlipped(_ sender: Any) {
+        if beaconSwitch.isOn {
+            isAnimating = true
+            self.appDelegate?.sendNotification()
+        }
+        else {
+            isAnimating = false
+        }
+    }
+    
+    
+    @objc func onTimer() {
+        if (isAnimating ?? false) {
+            let radarImage = UIImage(named: "Beacon Ring")
+            let radarImageView = UIImageView(image: radarImage)
+            radarImageView.frame = CGRect(x: 0.0, y: 0.0, width: 0.0, height: 0.0)
+            radarImageView.center = self.view.center
+            radarImageView.alpha = 1.0
+            self.view.addSubview(radarImageView)
+            self.view.sendSubviewToBack(radarImageView)
+            UIView.animate(withDuration: 3, animations: {
+                radarImageView.frame = CGRect(x: 0.0, y:(Double(self.screenHeight)/2)-(Double(self.screenWidth)/2), width: Double(self.screenWidth), height: Double(self.screenWidth))
+                radarImageView.alpha = 0.0
+            }) { (true) in
+                radarImageView.removeFromSuperview()
+            }
+        }
     }
     /*
     // MARK: - Navigation
